8e5824a7badbdf64aa190412c8f01615389860aa
Ankit <papai.ankit@gmail.com>
2024-12-29 09:55:39 +0530

    first commit

---
+++ converter.py
@@ -0,0 +1,24 @@
+from pillow_heif import register_heif_opener
+from PIL import Image
+import os
+
+
+register_heif_opener()
+
+class ImageConverter:
+    def __init__(self, input_path: str, output_path: str,quality: int = 95):
+        self.input_path = input_path
+        self.output_path = output_path
+        self.quality = quality
+
+    def convert(self):
+        try:
+            
+            image = Image.open(self.input_path)
+            output_dir = os.path.dirname(self.output_path)
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+                image.save(self.output_path, "JPEG",quality = self.quality)
+            print(f"Conversion successful! JPEG saved at: {self.output_path} with quality {self.quality}")
+        except Exception as e:
+            print(f"Error during conversion: {e}")
---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from converter import ImageConverter
+
+def main():
+    input_folder = os.path.join("images")  
+    output_folder = os.path.join("output") 
+
+    
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+   
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith(".heic"):
+            input_path = os.path.join(input_folder, filename)
+            output_path = os.path.join(output_folder, f"{os.path.splitext(filename)[0]}.jpeg")
+
+            print(f"Converting: {input_path} -> {output_path}")
+            
+            # Create a converter instance and convert
+            converter = ImageConverter(input_path, output_path,quality=100)
+            converter.convert()
+
+if __name__ == "__main__":
+    main()
